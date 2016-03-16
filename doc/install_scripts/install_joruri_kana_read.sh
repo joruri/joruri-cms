219c54df59bb44e69ae9dee308c1378320db9d33
norancer <hirao@sitebridge.co.jp>
2016-03-16 10:42:16 +0100

    Update documents

--- doc/install_scripts/install_joruri_kana_read.sh
+++ doc/install_scripts/install_joruri_kana_read.sh
@@ -13,44 +13,44 @@ ubuntu() {
 centos() {
   echo "It's CentOS!"
 
-  yum install -y glibc-devel.i686 libstdc++-devel.i686 sox
+  yum -y install sox
 
   cd /usr/local/src
-  rm -rf hts_engine_API-1.07.tar.gz hts_engine_API-1.07
-  wget http://downloads.sourceforge.net/hts-engine/hts_engine_API-1.07.tar.gz
-  tar zxf hts_engine_API-1.07.tar.gz && cd hts_engine_API-1.07 && ./configure CFLAGS='-m32' && make && make install
+  rm -rf hts_engine_API-1.09.tar.gz hts_engine_API-1.09
+  curl -fsSLO http://downloads.sourceforge.net/hts-engine/hts_engine_API-1.09.tar.gz
+  tar zxf hts_engine_API-1.09.tar.gz && cd hts_engine_API-1.09 && ./configure CFLAGS='-O3 -march=native -funroll-loops' && make && make install
 
   cd /usr/local/src
-  rm -rf open_jtalk-1.06.tar.gz open_jtalk-1.06
-  wget http://downloads.sourceforge.net/open-jtalk/open_jtalk-1.06.tar.gz
-  tar zxf open_jtalk-1.06.tar.gz && cd open_jtalk-1.06
+  rm -rf open_jtalk-1.08.tar.gz open_jtalk-1.08
+  curl -fsSLO http://downloads.sourceforge.net/open-jtalk/open_jtalk-1.08.tar.gz
+  tar zxf open_jtalk-1.08.tar.gz && cd open_jtalk-1.08
   sed -i 's/#define MAXBUFLEN 1024/#define MAXBUFLEN 10240/' bin/open_jtalk.c
-  ./configure --with-charset=UTF-8 CPPFLAGS='-m32' LDFLAGS='-m32' && make && make install
+  ./configure --with-charset=UTF-8 CFLAGS='-O3 -march=native -funroll-loops' CXXFLAGS='-O3 -march=native -funroll-loops' && make && make install
 
   cd /usr/local/src
-  rm -rf open_jtalk_dic_utf_8-1.06.tar.gz open_jtalk_dic_utf_8-1.06
-  wget http://downloads.sourceforge.net/open-jtalk/open_jtalk_dic_utf_8-1.06.tar.gz
-  tar zxf open_jtalk_dic_utf_8-1.06.tar.gz
-  mkdir /usr/local/share/open_jtalk && mv open_jtalk_dic_utf_8-1.06 /usr/local/share/open_jtalk/dic
+  rm -rf open_jtalk_dic_utf_8-1.08.tar.gz open_jtalk_dic_utf_8-1.08
+  curl -fsSLO http://downloads.sourceforge.net/open-jtalk/open_jtalk_dic_utf_8-1.08.tar.gz
+  tar zxf open_jtalk_dic_utf_8-1.08.tar.gz
+  mkdir /usr/local/share/open_jtalk && mv open_jtalk_dic_utf_8-1.08 /usr/local/share/open_jtalk/dic
 
   cd /usr/local/src
   rm -rf lame-3.99.5.tar.gz lame-3.99.5
-  wget http://jaist.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+  curl -fsSLO http://jaist.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
   tar zxf lame-3.99.5.tar.gz && cd lame-3.99.5 && ./configure && make && make install
 
   cd /usr/local/src
   rm -rf mecab-0.996.tar.gz mecab-0.996
-  wget http://mecab.googlecode.com/files/mecab-0.996.tar.gz
+  curl -fsSLO http://mecab.googlecode.com/files/mecab-0.996.tar.gz
   tar zxf mecab-0.996.tar.gz && cd mecab-0.996 && ./configure --enable-utf8-only && make && make install
 
   cd /usr/local/src
   rm -rf mecab-ipadic-2.7.0-20070801.tar.gz mecab-ipadic-2.7.0-20070801
-  wget http://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
+  curl -fsSLO http://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
   tar zxf mecab-ipadic-2.7.0-20070801.tar.gz && cd mecab-ipadic-2.7.0-20070801 && ./configure --with-charset=utf8 && make && make install
 
   cd /usr/local/src
   rm -rf mecab-ruby-0.996.tar.gz mecab-ruby-0.996
-  wget http://mecab.googlecode.com/files/mecab-ruby-0.996.tar.gz
+  curl -fsSLO http://mecab.googlecode.com/files/mecab-ruby-0.996.tar.gz
   tar zxf mecab-ruby-0.996.tar.gz && cd mecab-ruby-0.996 && ruby extconf.rb && make && make install
 }
 

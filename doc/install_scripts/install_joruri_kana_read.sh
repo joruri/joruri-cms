141084bcf3c93525c8483f86942ce84b8e341760
Yusuke Hirao <hirao@sitebridge.co.jp>
2016-07-13 19:38:54 +0900

    Change url to download the open_jtalk library

--- doc/install_scripts/install_joruri_kana_read.sh
+++ doc/install_scripts/install_joruri_kana_read.sh
@@ -17,19 +17,19 @@ centos() {
 
   cd /usr/local/src
   rm -rf hts_engine_API-1.09.tar.gz hts_engine_API-1.09
-  curl -fsSLO http://downloads.sourceforge.net/hts-engine/hts_engine_API-1.09.tar.gz
+  curl -fsSLO http://ufpr.dl.sourceforge.net/project/hts-engine/hts_engine%20API/hts_engine_API-1.09/hts_engine_API-1.09.tar.gz
   tar zxf hts_engine_API-1.09.tar.gz && cd hts_engine_API-1.09 && ./configure CFLAGS='-O3 -march=native -funroll-loops' && make && make install
 
   cd /usr/local/src
   rm -rf open_jtalk-1.08.tar.gz open_jtalk-1.08
-  curl -fsSLO http://downloads.sourceforge.net/open-jtalk/open_jtalk-1.08.tar.gz
+  curl -fsSLO http://ufpr.dl.sourceforge.net/project/open-jtalk/Open%20JTalk/open_jtalk-1.08/open_jtalk-1.08.tar.gz
   tar zxf open_jtalk-1.08.tar.gz && cd open_jtalk-1.08
   sed -i 's/#define MAXBUFLEN 1024/#define MAXBUFLEN 10240/' bin/open_jtalk.c
   ./configure --with-charset=UTF-8 CFLAGS='-O3 -march=native -funroll-loops' CXXFLAGS='-O3 -march=native -funroll-loops' && make && make install
 
   cd /usr/local/src
   rm -rf open_jtalk_dic_utf_8-1.08.tar.gz open_jtalk_dic_utf_8-1.08
-  curl -fsSLO http://downloads.sourceforge.net/open-jtalk/open_jtalk_dic_utf_8-1.08.tar.gz
+  curl -fsSLO http://ufpr.dl.sourceforge.net/project/open-jtalk/Dictionary/open_jtalk_dic-1.08/open_jtalk_dic_utf_8-1.08.tar.gz
   tar zxf open_jtalk_dic_utf_8-1.08.tar.gz
   mkdir /usr/local/share/open_jtalk && mv open_jtalk_dic_utf_8-1.08 /usr/local/share/open_jtalk/dic
 
